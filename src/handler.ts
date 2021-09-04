0278c50e1d77911124adda2d487f4e9de11cb785
Jacob Habib <habib.50@osu.edu>
2021-09-04 00:39:15 -0400

    Added CORS data

--- src/handler.ts
+++ src/handler.ts
@@ -1,4 +1,22 @@
 export async function handleRequest(request: Request): Promise<Response> {
+  const corsHeaders = {
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Methods': 'GET,OPTIONS',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Max-Age': '86400'
+  }
+  if (request.method === "OPTIONS") {
+    return new Response("OK", { headers: corsHeaders })
+  }
+
+  if (request.method === "GET") {
+    return getItemData(request)
+  } else {
+    return new Response('Unsupported request', { status: 500 })
+  }
+}
+
+const getItemData = async (request: Request) => {
   let response: Response
   switch (request.method == 'GET') {
     case request.headers.get('Key') === 'Coffee': {
@@ -6,7 +24,7 @@ export async function handleRequest(request: Request): Promise<Response> {
       const prices = await PRICES_DB.get('Coffee')
       return new Response(JSON.stringify(prices), {
         headers: {
-          'Content-type': 'application/json',
+          'Content-Type': 'application/json',
         },
       })
     }

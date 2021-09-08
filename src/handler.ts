61148d1e8b589d7e95abb1577ac3210086ad7d3c
Jacob Habib <habib.50@osu.edu>
2021-09-07 23:05:10 -0400

    added HEAD for CORS

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,7 @@
 export async function handleRequest(request: Request): Promise<Response> {
   const corsHeaders = {
     'Access-Control-Allow-Headers': '*',
-    'Access-Control-Allow-Methods': 'GET,OPTIONS',
+    'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Max-Age': '86400'
   }

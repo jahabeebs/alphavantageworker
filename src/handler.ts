a1b0d297335202c779566e4ec3b1d936a5074faa
Jacob Habib <habib.50@osu.edu>
2021-10-10 00:07:43 -0400

    added support for multiple commodities

--- src/handler.ts
+++ src/handler.ts
@@ -3,38 +3,43 @@ export async function handleRequest(request: Request): Promise<Response> {
     'Access-Control-Allow-Headers': '*',
     'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS',
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Max-Age': '86400'
+    'Access-Control-Max-Age': '86400',
   }
-  if (request.method === "OPTIONS") {
-    return new Response("OK", { headers: corsHeaders })
+  if (request.method === 'OPTIONS') {
+    return new Response('OK', { headers: corsHeaders })
   }
 
-  if (request.method === "GET") {
+  if (request.method === 'GET') {
     return getItemData(request)
   } else {
-    return new Response("Unsupported request", { status: 500 })
+    return new Response('Unsupported request', { status: 500 })
   }
 }
 
 const getItemData = async (request: Request) => {
   let response: Response
-  switch (request.method == "GET") {
-    case request.headers.get("Key") === "Coffee": {
+  const availableCommodities = ['Coffee', 'CoffeeArabica']
+  const headerKeyValue = request.headers.get('Key') as string
+  switch (request.method == 'GET') {
+    case availableCommodities.includes(headerKeyValue): {
       // @ts-ignore
-      const prices = await PRICES_DB.get("Coffee")
-      const pricesAsJSON = JSON.parse(prices);
+      const prices = await PRICES_DB.get(headerKeyValue)
+      const pricesAsJSON = JSON.parse(prices)
+      if (pricesAsJSON == null) {
+        return new Response('Data was null for given commodity', { status: 500 })
+      }
       return new Response(JSON.stringify(pricesAsJSON), {
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           'Access-Control-Allow-Headers': '*',
           'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS',
           'Access-Control-Allow-Origin': '*',
-          'Access-Control-Max-Age': '86400'
-        }
+          'Access-Control-Max-Age': '86400',
+        },
       })
     }
     default:
-      response = new Response("No data found for given item", { status: 500 })
+      response = new Response('No data found for given commodity', { status: 500 })
       break
   }
   return response
--- src/scheduledhandler.ts
+++ src/scheduledhandler.ts
@@ -1,22 +1,26 @@
 export async function scheduledHandleRequest(event: any) {
-  return new Promise((resolve, reject) => {
-    const resp = fetch('https://api.commoprices.com/v2/dataseries/INQJK/data', {
-      headers: {
+  return new Promise((resolve) => {
+    const commodityCode = ['INQJK', 'INQD4']
+    const databaseCode = ['Coffee', 'CoffeeArabica']
+    for (let i = 0; i < commodityCode.length; i++) {
+      const resp = fetch('https://api.commoprices.com/v2/dataseries/' + commodityCode[i] + '/data', {
+        headers: {
+          // @ts-ignore
+          Authorization: `Bearer ${API_KEY}`,
+          Accept: 'application/json',
+        },
+      }).then((respuesta) => respuesta.json())
+        .then((respuesta) => respuesta.data.request.dataseries.map((item: any) => ({
+          date: item[0],
+          price: item[1],
+        })))
         // @ts-ignore
-        Authorization: `Bearer ${API_KEY}`,
-        Accept: 'application/json',
-      },
-    }).then((respuesta) => respuesta.json())
-      .then((respuesta) => respuesta.data.request.dataseries.map((item: any) => ({
-        date: item[0],
-        price: item[1],
-      })))
-      // @ts-ignore
-      .then((respuesta) => PRICES_DB.put('Coffee', JSON.stringify(respuesta)))
-    if (resp == null) {
-      throw new Error('No data returned')
-    } else {
-      resolve(resp)
+        .then((respuesta) => PRICES_DB.put(databaseCode[i], JSON.stringify(respuesta)))
+      if (resp == null) {
+        throw new Error('No data returned')
+      } else {
+        resolve(resp)
+      }
     }
   })
 }
